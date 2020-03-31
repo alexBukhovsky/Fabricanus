973b688f3889c67c2151f59d436952a07d754634
PHILIPPS <sniper911185000@gmail.com>
2020-03-31 19:44:21 +0300

    test
    Signed-off-by: PHILIPPS <sniper911185000@gmail.com>

--- src/Staff.java
+++ src/Staff.java
@@ -7,17 +7,11 @@ public class Staff {
     private int price;
 
     Staff(String name, String description, String producer, int count, int price){
-
         this.name = name;
-
         this.description = description;
-
         this.producer = producer;
-
         this.count = count;
-
         this.price = price;
-
     }
 
     public String getName(){
