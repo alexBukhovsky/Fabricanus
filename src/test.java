b20485c431546d8a025d65a9cf8a7d9b1743660a
GitKovHub <gitkovhub@users.noreply.github.com>
2020-03-31 20:20:35 +0300

    gavnotester

--- src/test.java
+++ src/test.java
@@ -1,5 +1,38 @@
-public class test {
-    public static void main(String[] args) {
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+import javax.swing.border.Border;
 
+public class test extends JFrame{
+  private JButton button;
+
+
+    public test() {
+        super();
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.getContentPane().setLayout(new FlowLayout());
+        this.setSize(400, 150);
+
+        button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent arg0) {
+
+
+            }
+        });
+
+    }
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        test op = new test();
+        op.setVisible(true);
     }
 }
