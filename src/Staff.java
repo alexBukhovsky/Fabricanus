ab93a1c5ef8f92815f1cdb995797647110d8daed
GitKovHub <gitkovhub@users.noreply.github.com>
2020-03-31 19:15:08 +0300

    add method to staff

--- src/Staff.java
+++ src/Staff.java
@@ -1,4 +1,13 @@
 public class Staff {
+    String name;
+public String getName(){
+    return this.name;
+}
+    public String getPrice(){
+        return this.price;
+    }
 
-
+    public String getCount(){
+        return this.count;
+    }
 }
